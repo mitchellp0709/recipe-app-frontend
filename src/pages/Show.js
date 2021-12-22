b6f0188e382a0fde43fa1ae41740294fbc657e50
Mitchell Paoletti <mitchellp0709@gmail.com>
2021-12-22 10:59:29 -0500

    fixed show page issue

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,17 +1,38 @@
-import { useQuery } from "@apollo/client"
-import { GET_RECIPE } from "../graphql/Queries"
+import { useMutation, useQuery } from "@apollo/client"
+import { GET_RECIPE, GET_RECIPES } from "../graphql/Queries"
 import { useState } from "react"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, useNavigate } from "react-router-dom"
+import { DELETE_RECIPE } from "../graphql/Mutations"
 
-const Show = ({getQuery}) => {
+
+const Show = ({ getQuery }) => {
+  /////////////////////////
+  //Define dependencies
+  /////////////////////////
+  const navigate = useNavigate();
   const params = useParams();
   const id = params.id;
-  const [recipe, setRecipe] = useState({})
+  const [recipe, setRecipe] = useState({});
 
+  /////////////////////////
+  //Create Functions
+  /////////////////////////
 
-  const { loading, error, data } =  useQuery(GET_RECIPE, {
-    variables: { id: id },onCompleted:(data)=> setRecipe(data.getRecipe)
-  })
+  const [deleteRecipe] = useMutation(DELETE_RECIPE);
+  const removeRecipe = (id) => {
+    deleteRecipe({
+      variables: {
+        id: id,
+      },
+      refetchQueries: [{ query: GET_RECIPES }],
+    });
+    navigate("/");
+  };
+
+  const { loading, error, data } = useQuery(GET_RECIPE, {
+    variables: { id: id },
+    onCompleted: (data) => setRecipe(data.getRecipe),
+  });
   // const { loading, error, data }=getQuery(id)
 
   if (loading) return <h1>loading...</h1>;
@@ -24,20 +45,34 @@ const Show = ({getQuery}) => {
         <h1>{recipe.name}</h1>
         <img src={recipe.image} alt="pic of food" />
         <h2>{recipe.description}</h2>
-        {/* <div className="doubleList">
-          <ul>
-            {recipe.quantities.map((x) => {
-              return <li>{x}</li>;
-            })}
-          </ul>
-          <ul>
-            {recipe.ingredients.map((x) => {
-              return <li>{x}</li>;
-            })}
-          </ul>
-        </div> */}
+
+        {recipe.quantities ? (
+          <div className="doubleList">
+            <ul>
+              {recipe.quantities.map((x) => {
+                return <li>{x}</li>;
+              })}
+            </ul>
+            <ul>
+              {recipe.ingredients.map((x) => {
+                return <li>{x}</li>;
+              })}
+            </ul>
+          </div>
+        )  : (
+          <h1>Loading...</h1>
+        )}
+        
         <p>{recipe.instructions}</p>
-        <Link to ={`/${id}/edit`}><button>Edit</button></Link>
+        <Link to={`/${id}/edit`}>
+          <button>Edit</button>
+        </Link>
+        <button
+          onClick={() => {
+            removeRecipe(id);
+          }}>
+          Delete
+        </button>
       </div>
     );
   }
