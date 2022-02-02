ba0c625c0a20052a2fdf7f89ffc3196e295fed9b
Mitchell Paoletti <mitchellp0709@gmail.com>
2022-02-02 16:10:29 -0500

    fixed random recipe issue

--- src/pages/Random.js
+++ src/pages/Random.js
@@ -11,18 +11,19 @@ const Random = (props) => {
 
   useEffect(() => getRandomRecipe(),[])
 
-  
-  return <div>
-    <h1>{randomRecipe.recipes[0].title}</h1>
-    <img src={randomRecipe.recipes[0].image} alt={randomRecipe.recipes[0].title} />
-    <h2>Ingredients: </h2>
-    <ul>
-      {randomRecipe.recipes[0].extendedIngredients.map((ing) => {
-        return <li>{ing.amount} {ing.unit} {ing.name}</li>
-      })}
-    </ul>
-    <h2>Instructions: </h2>
-    <p>{randomRecipe.recipes[0].instructions}</p>
-  </div>
+  if (randomRecipe) {
+    return <div>
+      <h1>{randomRecipe.recipes[0].title}</h1>
+      <img src={randomRecipe.recipes[0].image} alt={randomRecipe.recipes[0].title} />
+      <h2>Ingredients: </h2>
+      <ul>
+        {randomRecipe.recipes[0].extendedIngredients.map((ing) => {
+          return <li>{ing.amount} {ing.unit} {ing.name}</li>
+        })}
+      </ul>
+      <h2>Instructions: </h2>
+      <p>{randomRecipe.recipes[0].instructions}</p>
+    </div>
+  } else{return <h1>Loading...</h1>}
 }
 export default Random
